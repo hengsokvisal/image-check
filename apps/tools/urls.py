629f56daf124eb4412e4d7f2c38edfec0eca8a4a
monyoudom <thongmonyoudom@outlook.com>
2018-08-21 14:22:20 +0700

    Basic auth and count missing image

--- apps/tools/urls.py
+++ apps/tools/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name="index"),
+    path('login_user',views.login_user,name="login_user"),
 ]
--- apps/tools/views.py
+++ apps/tools/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render ,HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
 from .helper import addDict
 
 import json
@@ -8,34 +12,36 @@ from ImageCheck import  mainCheck
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-loaFile = glob.glob(BASE_DIR+"/ImageCheck/MissingJSON/*.json")
+
 
 def index(request):
     context =  {}
-    if request.method == "POST":
-        if (request.POST['json_type'] != "result"):
-            print("okay")
-            context = load_json()
-            context =  {
-                'result' : context['result'][request.POST['json_type']],
-                'type'  : request.POST['json_type']
-            }
-            return render(request,"detail.html",context)
-        else:
-            print("why")
-            mainCheck.main()
-            context = load_json()
-            return render(request,"result.html",context)
+    if not request.user.is_authenticated:
+        return render(request,"login.html")
+    else:
+        if request.method == "POST":
+            if (request.POST['json_type'] != "result"):
+                context = load_json()
+                context =  {
+                    'result' : context['result'][request.POST['json_type']],
+                    'type'  : request.POST['json_type']
+                }
+                return render(request,"detail.html",context)
+            else:
+                mainCheck.main()
+                context = load_json()
+                return render(request,"result.html",context)
     return render(request , "index.html",context)
     
 
 def load_json():
+    loaFile = glob.glob(BASE_DIR+"/ImageCheck/MissingJSON/*.json")
     add_dict = addDict()
-    
-    for i in loaFile:
+    print(loaFile)
+    for file_json in loaFile:
         try:
-            with open(i) as handle:
-                json_name = i.replace(BASE_DIR+"/ImageCheck/MissingJSON/","")
+            with open(file_json) as handle:
+                json_name = file_json.replace(BASE_DIR+"/ImageCheck/MissingJSON/","")
                 json_type = json_name.replace(".json","")
                 add_dict.add(json_type,json.loads(handle.read()))
         except:
@@ -44,3 +50,18 @@ def load_json():
         'result' : add_dict
     }
     return context
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect("/")
+            else:
+                return render(request, 'login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'login.html', {'error_message': 'Invalid login'})
+    return render(request, 'login.html')

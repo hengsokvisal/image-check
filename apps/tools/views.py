4f00a38c3c6c602bccb5179fe35835a727703e92
monyoudom <thongmonyoudom@outlook.com>
2018-08-21 12:51:08 +0700

    fixing miss json

--- apps/tools/views.py
+++ apps/tools/views.py
@@ -6,35 +6,41 @@ import os
 import glob
 from ImageCheck import  mainCheck
 
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 loaFile = glob.glob(BASE_DIR+"/ImageCheck/MissingJSON/*.json")
 
 def index(request):
-    add_dict = addDict()
-    for i in loaFile:
-        with open(i) as handle:
-            json_name = i.replace(BASE_DIR+"/ImageCheck/MissingJSON/","")
-            json_type = json_name.replace(".json","")
-            add_dict.add(json_type,json.loads(handle.read()))
-    context =  {
-        'result' : add_dict
-    }
-    print(request.method)
+    context =  {}
     if request.method == "POST":
-        print()
         if (request.POST['json_type'] != "result"):
-
+            print("okay")
+            context = load_json()
             context =  {
-                'result' : add_dict[request.POST['json_type']],
+                'result' : context['result'][request.POST['json_type']],
                 'type'  : request.POST['json_type']
             }
             return render(request,"detail.html",context)
         else:
-            
+            print("why")
             mainCheck.main()
-            print("Done ============")
+            context = load_json()
             return render(request,"result.html",context)
-
-
     return render(request , "index.html",context)
     
+
+def load_json():
+    add_dict = addDict()
+    
+    for i in loaFile:
+        try:
+            with open(i) as handle:
+                json_name = i.replace(BASE_DIR+"/ImageCheck/MissingJSON/","")
+                json_type = json_name.replace(".json","")
+                add_dict.add(json_type,json.loads(handle.read()))
+        except:
+            pass
+    context =  {
+        'result' : add_dict
+    }
+    return context
